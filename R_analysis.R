1fa45292009dffedab748b3c4f3d8b185810b2d9
Joel Obadiah <joelobadiahdelle@gmail.com>
2024-08-06 10:58:19 +0100

    cleaned the code for better understanding

--- R_analysis.R
+++ R_analysis.R
@@ -1,9 +1,12 @@
+#loading necessary libraries
 library(zoo)
 library(reshape2)
 library(forecast)
 library(tseries)
 
 data = read.csv("C:/Users/BIU LGA/Documents/Rain_Data.csv")
+
+#Reshaping data for easy analysis
 
 long_data = melt(data, id.vars = "YEAR", variable.name = "Month", value.name = "Rainfall")
 
@@ -19,17 +22,31 @@ rainfall_zoo = zoo(aggregated_data$Rainfall, order.by=aggregated_data$Date)
 
 rainfall_ts = ts(rainfall_zoo, frequency=12)
 
+#Checking for seasonality in data...
+
 plot(rainfall_ts, main="Monthly Rainfall Time Series", xlab="Date", ylab="Rainfall (mm)", col="blue")
 
 ggtsdisplay(rainfall_ts)
 
-#Performimg Augmented Dickey-Fuller test
+#we observe that the original data exhibits a wave like pattern evidence of seasonality 
+#and no trend is observed which implies that the Time series is stationary'''
+#From the trend of the data there is decrease in rainfall with some months having zero values.
+
+
+#Performing Augmented Dickey-Fuller test
 Adf_test = adf.test(rainfall_ts)
 print(Adf_test)
+
+##Alternative hypothesis: stationary
+##Conclusion: Since p-value < α, we reject H0 and conclude that the series is stationary
+
 
 #performing KPSS test for level stationarity
 kpss_test = kpss.test(rainfall_ts)
 print(kpss_test)
+
+##Conclusion: since p-value > α we do not reject H0. 
+##Hence, we conclude that the series is stationary.
 
 # Plot ACF and PACF
 par(mfrow=c(2,1))
@@ -37,15 +54,25 @@ acf(rainfall_ts, main="ACF of Rainfall Time Series")
 pacf(rainfall_ts, main="PACF of Rainfall Time Series")
 par(mfrow=c(1,1))
 
-#Fitting a SArima Model
+##The sinusoidal or periodic pattern in the ACF plot is again suggesting 
+##that the series has a strong seasonal effect also, the PACF plot is neither suggesting otherwise.
+
+#Fitting a Sarima Model
 sarima_model = auto.arima(rainfall_ts, seasonal = TRUE)
- #print summary
+ 
+#print summary
 summary(sarima_model)
 
-#check residua;s
+#checking for residuals
 checkresiduals(sarima_model)
 
-#forcasting values
+##Residuals shows the time series of the standardized residuals. The plot depicts no pattern, a clear 
+##indication that the standardized residuals behave like a typical white noise process as required.
+
+#forecasting values
 forecast = forecast(sarima_model,h=24)
 plot(forecast, main= "sarima model forecast")
 print(forecast)
+
+##As we can see from the forecast, May – September has the highest predicted 
+##values which indicates the beginning and peak of raining season in Jos.
